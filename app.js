15329d3ca2f375c6ae10f4566d02274ca79161d4
wisnu <wisnuajisaputrasec@gmail.com>
2023-08-26 11:32:09 +0700

    Add files app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// Print Hello World 10x
+
+const sayHelloWorld = "Hello Wolrd!"
+
+function printHelloWorld(){
+    for(let i = 0; i < 10; i++){
+        console.log(sayHelloWorld);
+    }
+}
+printHelloWorld();
