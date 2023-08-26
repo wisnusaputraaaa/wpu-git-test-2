4a93e85c478b5b749f8d28b1ddb7c8866e930826
wisnu <wisnuajisaputrasec@gmail.com>
2023-08-26 11:53:37 +0700

    Add a some code in sayName.js

---
+++ sayName.js
@@ -0,0 +1,12 @@
+function sayName(yourName){
+console.log(`Hello ${yourName}`)
+}
+
+sayName("Wisnu");
+
+function sum(num1, num2){
+    const total = num1 + num2;
+    return total;
+}
+
+console.log(sum(12,12));
